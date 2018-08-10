a2a010fa261555067070a8f8f3bf050c7c726142
Filip Krestan <krestfi1@fit.cvut.cz>
2018-08-10 23:44:30 +0200

    Remove NEMEA endpoint comment

--- blooming_history_aggregator/api.py
+++ blooming_history_aggregator/api.py
@@ -147,7 +147,6 @@ def get_merged(directory, timestamp_from, timestamp_to):
     return make_response(merged, 200, GET_MERGED_HEADERS)
 
 
-# FIXME fix url in NEMEA module
 # TODO user auth - hmac could be enough for starters
 @app.route('/<uuid:uuid_>/<int:timestamp_from>/<int:timestamp_to>/', methods=['GET', 'POST'])
 def ednpoint_bloom(uuid_, timestamp_from, timestamp_to):
